3f733d2c5d6654514951cde4e73084cb22d79b5d
Arshnoor kirmani <study.arshnoorkirmani@gmail.com>
2025-05-31 17:54:32 +0530

    Create a Admin Students section

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -3,5 +3,7 @@
 import AdminLayout from "./Layouts/AdminLayout";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import Profile from "./Pages/Profile/Profile";
+import Students from "./Pages/Students/Students";
+// ======================
 import Comming from "./Pages/Comming/Comming";
-export { AdminLayout, Dashboard, Profile, Comming };
+export { AdminLayout, Dashboard, Profile, Comming, Students };
